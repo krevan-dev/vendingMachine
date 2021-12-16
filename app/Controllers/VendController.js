18b11bfbaaec4195004b33c9206244378af80d39
Kyle Evans <krevan88@pm.me>
2021-12-15 21:04:13 -0700

    populated vending items

--- app/Controllers/VendController.js
+++ app/Controllers/VendController.js
@@ -1,16 +1,17 @@
 import { ProxyState } from "../AppState.js";
 import { vendService } from "../Services/VendService.js";
 
-function drawSnacks() {
+function _drawSnacks() {
+  const snacks = ProxyState.Snack;
   let template = "";
-  let snacks = ProxyState.Snack;
-  console.log(ProxyState.Snack);
+  snacks.forEach((s) => (template += s.Template));
+  document.getElementById("selections").innerHTML = template;
 }
 
 export class VendController {
   constructor() {
     console.log("VendController loaded.");
-    drawSnacks();
+    _drawSnacks();
   }
 
   addCredits() {
--- app/Models/Snack.js
+++ app/Models/Snack.js
@@ -4,4 +4,13 @@ export class Snack {
     this.price = data.price;
     this.img = data.img;
   }
+
+  get Template() {
+    return `
+      <div class="col-4 text-center my-4">
+        <img class="vendImg" src="${this.img}" alt="${this.name}" />
+        <h5 class="mt-2">${this.name} - $${this.price} </h5>
+      </div>
+    `;
+  }
 }
